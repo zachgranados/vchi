12e4fd70e1b8dec34c2fccfceaf680e49be937b9
Zachary Granados <zcbg@umich.edu>
2025-02-21 13:32:45 -0500

    updated database and export to excel

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import ebp_finder as ebp
 import provider_db_setup as setup
+
 # ebp.write_html()
 
 # creates dictionary with info
@@ -7,4 +8,8 @@ providers = ebp.collect_provider_info("static_ebp.txt")
 
 # creates db 
 setup.create_db()
+
+
+
+
 
